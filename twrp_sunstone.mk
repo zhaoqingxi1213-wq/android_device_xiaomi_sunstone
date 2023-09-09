f3c130521d975dbbd04daf0ad3b450e521b11408
zhaoqingxi1213-wq <z1936155673@gmail.com>
2023-09-09 11:43:34 +0800

    Update twrp_sunstone.mk

--- twrp_sunstone.mk
+++ twrp_sunstone.mk
@@ -10,9 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from sunstone device
 $(call inherit-product, device/qualcomm/sunstone/device.mk)
